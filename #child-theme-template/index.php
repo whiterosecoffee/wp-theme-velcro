56a3b11c07ee87dc86f89ce3488c84fb4500bb76
whiterosecoffee <fivetolife@gmail.com>
2016-08-25 16:21:24 +0700

    Include Child theme template

---
+++ #child-theme-template/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div id="content" class="content contentPadding floatfix">
+	<h2 class="pageTitle"><?php echo (the_parent_slug()); ?></h2>
+	<?php			
+        $page = $post->ID;
+        $page_articleDate = get_page($page);
+        $content = apply_filters('the_content', $page_articleDate->post_content);
+        echo $content; 
+    ?>
+    <? get_template_part("/section-templates/".the_parent_slug()."-content"); ?>
+
+</div>
+<?php get_footer(); ?>
+<!-- Child Theme Index Template -->
---
+++ #child-theme-template/section-templates/footer-content.php
@@ -0,0 +1,18 @@
+ <footer id="footerContent" class="content floatfix text-center">
+
+ 	<h2> Professional Estate <br/>Development <i class="icon icon-plus"></i> Consultancy</h2>
+ 	<h3>Creating Selective Sustained Projects</h3>
+ 	<h3>Realizing Your Dream House</h3>
+
+	<a class="mailto" href="mailto:sebastian@houseonphukent.com?subject=Website%20Contact">
+		<span class="icon icon-envelop"></span>
+		sebastian@houseonphuket.com
+	</a>
+
+ 	<p class="footerKeywords">
+ 		www.houseonphuket.com, House on phuket, architectural, interior, landscape, CAD, investment, project-consultancy
+ 	</p>
+
+    <a href="#" id="btn-to-top" class="scrollToTop icon-circle-up"></a>
+
+</footer>
